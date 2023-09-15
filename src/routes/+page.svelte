624bf71e4d0aaa6ecd4f5009a700375b2117a0bb
CaiqueMF <josecafran.militao1@gmail.com>
2023-09-15 11:16:05 -0300

    base

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<div class="cabecalho">
+    <h2>OVO!</h2>
+    <a href="/credito">creditos</a>
+</div>
+<div class="centro">
+    <h1>Ovoventuras de Ovaldo</h1>
+    <h2>A FAZENDA</h2>
+    <a href="/teste">INICIAR</a>
+</div>
+
+
+
+<style>
+
+</style>
---
+++ src/routes/credito/+page.svelte
@@ -0,0 +1,39 @@
+<script>
+    import celeiro from '../../assets/celeiro.png'
+    import ovo from '../../assets/ovo.png'
+    import cachorro from '../../assets/cachorro.png'
+    import fazendeiro from '../../assets/fazendeiro.png'
+    import fundo from '../../assets/fundo.png'
+</script>
+<img class="fundo" src={celeiro} alt="celeiro">
+<div class="cabecalho">
+    <a href="/">voltar</a>
+    <h2>OVO!</h2>
+</div>
+<div class="centro">
+    <img src={fundo} alt="fundo">
+    <ul>
+        <li>Equipe: OVO!</li>
+        <li> Caique França (554935)</li>
+        <li> David Silva (555735)</li>
+        <li> Márcio Freires (554069)</li>
+        <li> Milena Costa(553713)</li>
+        <li> Rafaely Sombra (552241)</li>
+        <li> Levi Ribeiro (552849)</li>
+    </ul>
+    <ul class="horizontal">
+        <li><img src={ovo} alt="ovo"></li>
+        <li><img src={cachorro} alt="cachorro"></li>
+        <li><img src={fazendeiro} alt="fazendeiro"></li>
+        <li><img src={celeiro} alt="celeiro"></li>
+        <ul>
+            <li><a href="https://www.vniuqu.com/?product_id=174309900_51">https://www.vniuqu.com/?product_id=174309900_51</a></li>
+            <li><a href="https://www.pngwing.com/en/free-png-nlbej">https://www.pngwing.com/en/free-png-nlbej</a></li>
+            <li><a href="https://www.pngwing.com/en/free-png-ijorq">https://www.pngwing.com/en/free-png-ijorq</a></li>
+            <li><a href="https://eduilsonduarte.artstation.com/projects/W2D4AD">https://eduilsonduarte.artstation.com/projects/W2D4AD</a></li>
+        </ul>
+    </ul>
+    <h2>Bibliografia</h2>
+    <a href="https://www.ufrgs.br/psicoeduc/behaviorismo/maquina-de-ensinar-de-skinner-1/">https://www.ufrgs.br/psicoeduc/behaviorismo/maquina-de-ensinar-de-skinner-1/</a>
+    <a href="https://ctd-smd.wixsite.com/cognosis2023-2/waldenii">https://ctd-smd.wixsite.com/cognosis2023-2/waldenii</a>
+</div>
